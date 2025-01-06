be821247ff5845291d95136efa1b32f70b167790
nehemiasmunoz <munoz_mar@outlook.com>
2025-01-06 11:32:51 -0300

    add: constructor y get/set

--- src/main/java/com/marvic/forohub/entities/course/Course.java
+++ src/main/java/com/marvic/forohub/entities/course/Course.java
@@ -12,4 +12,31 @@ public class Course {
     @Column(nullable = false, length = 90)
     private String category;
 
+    public Course() {
+    }
+
+    public Course(String name, String category) {
+        this.name = name;
+        this.category = category;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
